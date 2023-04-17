bf14c3ff9c32c76466760e762a4a51694a023bb8
marinfake <11218484@st.neu.edu.vn>
2023-04-16 21:07:21 -0700

    first commit

---
+++ assets/main.js
@@ -0,0 +1,24 @@
+$(window).scroll(function(){
+    if($(window).scrollTop()){
+         $("nav").addClass("scrolled");
+    }
+    else{
+        $("nav").removeClass("scrolled");
+
+    }
+});
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElement = document.querySelectorAll('.hidden');
+hiddenElement.forEach((el) => observer.observe(el));
